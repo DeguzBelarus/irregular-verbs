6a73704a30cc107ebe19a1782865c437a137cfa1
DeguzBelarus <deguz@mail.ru>
2024-03-21 15:11:46 +0300

    fix: fix mobile scrolling issue

--- src/config/GSAP/config.ts
+++ src/config/GSAP/config.ts
@@ -3,10 +3,8 @@ import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Draggable } from 'gsap/Draggable';
 
-import { IS_DESKTOP } from 'src/constants/constants';
 import { GSAPEasingEnum } from 'src/constants/gsapConstants';
 
 const { defaults, registerPlugin } = gsap;
 defaults({ ease: GSAPEasingEnum.NONE });
 registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
-ScrollTrigger.normalizeScroll(!IS_DESKTOP);
